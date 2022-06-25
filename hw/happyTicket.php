7e566bff1e1482b28e5e88c8410c5f4974400674
DobrelyaVladimir <vovchick1984@gmail.com>
2022-06-25 12:06:13 +0300

    all done

--- hw/happyTicket.php
+++ hw/happyTicket.php
@@ -1,36 +1,43 @@
 <?php
 
-//if (isset($_GET['min']) && isset($_GET['max'])) {
-//    $regex = "/^\d{6}$/";
-//    $res1 = preg_match($regex, (int)$_GET["min"]);
-//    $res2 = preg_match($regex, (int)$_GET["max"]);
+if (isset($_GET['min']) && isset($_GET['max'])) {
+    $regex = "/^\d{6}$/";
+    $res1 = preg_match($regex, sprintf('%06d', (int)$_GET["min"]));
+    $res2 = preg_match($regex, sprintf('%06d', (int)$_GET["max"]));
     $count = 0;
     header("Content-Type: application/json");
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST');
     header("Access-Control-Allow-Headers: X-Requested-With");
-   // if ($res1 && $res2) {
-//        $min = $_GET['min'];
-//        $max = $_GET['max'];
-        $min = '000001';
-        $max = '001001';
+    if ($res1 && $res2) {
+        $min = $_GET['min'];
+        $max = $_GET['max'];
 
         for ($i = (int)$min; $i <= (int)$max; $i++) {
             $str = sprintf('%06d', $i);
-            var_dump($i);
             $arr = str_split($str);
             $arr = array_chunk($arr, 3);
             $arrA = $arr[0];
             $arrB = $arr[1];
-            while (count($arrA) > 1 || count($arrB) > 1) {
+            $arrA = array_sum($arrA);
+            $arrB = array_sum($arrB);
+
+            while (strlen((string)$arrA) > 1) {
+                $arrA = str_split($arrA);
                 $arrA = array_sum($arrA);
+
+            }
+            while (strlen((string)$arrB) > 1) {
+                $arrB = str_split($arrB);
                 $arrB = array_sum($arrB);
             }
+
             if ($arrA === $arrB) {
                 $count++;
             }
         }
-            var_dump($count);
-        //echo json_encode($count);
-    //}
-//}
+
+
+    }
+    echo json_encode($count);
+}
--- hw/index.php
+++ hw/index.php
@@ -10,6 +10,7 @@
 </head>
 <body>
 <div class="container">
+    <h2>Поиск количества счастливых билетов в диапазоне чисел</h2>
     <form id="main">
         <p>Введите два числа в формате ХХХХХХ</p>
         <div class="minDiv">
@@ -19,6 +20,7 @@
         <div class="maxDiv">
             <input class="max" type="text" name="max" placeholder="max">
             <p id="err2" class="hide">"Число введено не верно"</p>
+            <p id="err3" class="hide">"Число min должно быть меньше числа max"</p>
         </div>
         <button type="submit">Run</button>
     </form>
@@ -31,26 +33,37 @@
     let maxDiv = document.querySelector(".maxDiv");
     let message = document.querySelector("#err1");
     let message2 = document.querySelector("#err2");
+    let message3 = document.querySelector("#err3");
+    let tickets = document.createElement("div");
 
     form.addEventListener("submit", async function (e) {
         e.preventDefault();
+        tickets.innerHTML = ``;
         message.classList.remove("error");
         message2.classList.remove("error");
+        message3.classList.remove("error");
         const regex = /^\d{6}$/;
         let err = 0;
         if (!regex.test(min.value)) {
             message.classList.add("error");
-            err++
+            err++;
         }
         if (!regex.test(max.value)) {
             message2.classList.add("error");
-            err++
+            err++;
         }
-        if (err == 0) {
+        if (min.value > max.value) {
+            message3.classList.add("error");
+            err++;
+        }
+        if (err === 0) {
             let response = await fetch(`happyTicket.php?min=${min.value}&max=${max.value}`);
-
             let res = await response.json();
-            console.log(res);
+            if (res) {
+
+               tickets.innerHTML = `Количество счастливых билетов: ${res} шт.`;
+               form.append(tickets);
+            }
         }
     })
 </script>
